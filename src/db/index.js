e67b3b75906cb48ab4a036c478d4431c238e88e2
itesh <itesh1122.ac@gmail.com>
2023-12-08 09:54:12 +0530

    connect to mongodb

---
+++ src/db/index.js
@@ -0,0 +1,12 @@
+import mongoose from "mongoose";
+import { DB_NAME } from "../constants.js";
+const connetDb = async () => {
+    try {
+        const conn = await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`);
+        console.log(`MongoDB connected: ${conn.connection.host}`);
+    } catch (error) {
+        console.log(error);
+        process.exit(1);
+    }
+}
+export default connetDb
---
+++ src/index.js
@@ -0,0 +1,28 @@
+import dotenv from "dotenv";
+import connetDb from "./db/index.js";
+
+dotenv.config(
+    {
+        path: "./env"
+    }
+);
+
+connetDb()
+
+/*
+const app = express(); //app
+(async () => {
+    try {
+        await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`);
+        app.on("error", (error) => {
+            console.log(error);
+            throw error;
+        })
+        app.listen(process.env.PORT, () => {
+            console.log(`server is running on port ${process.env.PORT}`);
+        })
+    } catch (error) {
+        console.log(error);
+    }
+})
+*/
